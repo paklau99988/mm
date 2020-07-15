36ae35a412fc5399cd0fa7800354c0663f4180a1
paklau99988 <65712388+paklau99988@users.noreply.github.com>
2020-07-15 15:03:57 +0300

    Update ins.sh

--- mm/mm-ins/ins.sh
+++ mm/mm-ins/ins.sh
@@ -1,18 +1,21 @@
-pip uninstall -y mmdet mmcv torch torchvision 
-conda install -y cython==0.28.5
-pip install "git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI"
-pip install torch==1.4.0 torchvision==0.5.0 -f https://download.pytorch.org/whl/torch_stable.html
-#pip install torch==1.5.0 torchvision==0.6.0
-#conda install -y  pytorch cudatoolkit=10.2 torchvision -c pytorch
-pip install -q  terminaltables==3.1.0 Pillow==6.2.2
-git clone --recursive 'https://github.com/open-mmlab/mmdetection.git'
-cd mmdetection
-rm -rf build
-#pip install -r "requirements/build.txt"
-#pip install -q mmcv==0.2.16
-pip install -v -e .
-#python setup.py install
-#python setup.py develop
+export TORCH_CUDA_ARCH_LIST="6.0 6.1 7.0+PTX"
+export  TORCH_NVCC_FLAGS="-Xfatbin -compress-all"
+export  CMAKE_PREFIX_PATH="$(dirname $(which conda))/../"
 
-#pip install -q mmcv==0.2.16
-pip install -q mmcv==0.6.2
+
+pip uninstall -y mmdet mmcv
+conda install -y cython==0.28.5
+#pip install mmcv==0.2.15
+pip install mmcv==0.6.2 terminaltables Pillow==6.2.2
+
+export FORCE_CUDA="1"
+
+pip install "git+https://github.com/open-mmlab/cocoapi.git#subdirectory=pycocotools"
+pip install "git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI"
+#git clone https://github.com/open-mmlab/mmdetection.git mmdetection
+#cd mmdetection
+rm -rf build
+pip install -r requirements/build.txt
+python setup.py develop
+pip install --no-cache-dir -e .
+pip install mmcv==0.6.2 mmcv-full
